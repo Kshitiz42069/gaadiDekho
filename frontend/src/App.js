42ba8475a97181ccda0b9c7f987b84412f86c8ba
KSHITIZ RAI <kshitizroy836@gmail.com>
2024-11-15 13:11:10 +0530

    complete setup

---
+++ frontend/src/App.js
@@ -0,0 +1,11 @@
+
+
+function App() {
+  return (
+    <div className="App">
+      <p className="text-[40px] font-[700]">Hey There! Lets solve this assessment and submit it as soon as possible.</p>
+    </div>
+  );
+}
+
+export default App;
